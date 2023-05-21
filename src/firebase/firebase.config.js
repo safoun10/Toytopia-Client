99caa805f081cf713e4f08c9733cf96b8e0141dd
Safoun <safoun.official@gmail.com>
2023-05-21 08:35:27 +0600

    env var added in client side

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCOVEEgE8jCi0IdKWuRuDk52SUqMpENznM",
-    authDomain: "toytopia-safoun.firebaseapp.com",
-    projectId: "toytopia-safoun",
-    storageBucket: "toytopia-safoun.appspot.com",
-    messagingSenderId: "523270093006",
-    appId: "1:523270093006:web:243639f52c9d2b4ad4f89f"
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID
 };
 
 const app = initializeApp(firebaseConfig);
